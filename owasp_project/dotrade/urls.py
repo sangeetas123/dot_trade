a61c35d00088b86e23be9f2fbca65605ab70b0f9
Sangeeta Singh <sangeetasingh@Sangeetas-MacBook-Air.local>
2023-03-14 16:33:13 +0530

    password reset

--- owasp_project/dotrade/urls.py
+++ owasp_project/dotrade/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls import include
 
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -9,4 +10,8 @@ urlpatterns = [
     path('accounts/', include("django.contrib.auth.urls")),
     path('logout', views.logout_view, name='customlogout'),
     path('accounts/signup', views.signupView, name='signup'),
+    path('accounts/reset/done',
+        auth_views.PasswordResetDoneView.as_view(template_name='registration/reset_complete.html'),
+        name='password_reset_complete'),
+
 ]
