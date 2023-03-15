4e9e049229ec0ad008b5092127c8bd8630935f8a
Sangeeta Singh <sangeetasingh@Sangeetas-MacBook-Air.local>
2023-03-15 19:15:44 +0530

    remove unnecessary code

--- owasp_project/owasp_project/views.py
+++ owasp_project/owasp_project/views.py
@@ -1,20 +1,16 @@
 from django_ratelimit.decorators import ratelimit
 from django_ratelimit.exceptions import Ratelimited
 
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.contrib import messages
 
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponseForbidden
 
 def login_wrapper(login_func):
 
     @ratelimit(key='ip', method='POST', rate='2/5m')
     def admin_login(request, **kwargs):
-        if getattr(request, 'limited', False):  # was_limited
-            messages.error(request, 'Too many login attempts, please wait 5 minutes')
-            return redirect('/dotrade')
-        else:
-            return login_func(request, **kwargs)
+        return login_func(request, **kwargs)
 
     return admin_login
 
